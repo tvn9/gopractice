a3d3b78c604e307369bb25beea389bde6ce117cb
Thanh Nguyen <chrisng9@outlook.com>
2022-01-23 21:41:03 -0800

    Adding pass by pointer in method receiver

--- methods/book.go
+++ methods/book.go
@@ -5,29 +5,48 @@ import (
 	"time"
 )
 
+type price struct {
+	price     float32
+	priceDn   float32
+	priceUp   float32
+	salePrice float32
+}
+
 type Book struct {
 	ISBN      string
 	Title     string
-	Price     float32
+	Price     price
 	Author    string
 	PubDate   time.Time
 	Publisher string
 }
 
-type ListBooks []Book
+type ListBooks []*Book
 
 // PrintItem method print a single book with any idividual field
 func (b *Book) PrintItem() {
-	fmt.Printf("Book Title: %-20s Price %.2f\n", b.Title, b.Price)
+	fmt.Printf("Book Title: %-20s Price %.2f\n", b.Title, b.Price.price)
 }
 
 // PrintList method print a slice of books
 func (l *ListBooks) PrintList() {
 	if len(*l) > 0 {
 		for i, b := range *l {
-			fmt.Printf("#%d Title: %-20s Price: %.2f\n", i, b.Title, b.Price)
+			fmt.Printf("#%d Title: %-20s Price: %.2f, Discount: %.2f, Inscrease: %.2f, Your Price: %.2f\n", i, b.Title, b.Price.price, b.Price.priceDn, b.Price.priceUp, b.Price.salePrice)
 		}
 	} else {
 		fmt.Println("The is no book in the list!")
 	}
+}
+
+// discount
+func (p *Book) discount(d *float32) float32 {
+	p.Price.salePrice = p.Price.price * (1 - *d)
+	return p.Price.salePrice
+}
+
+// increase
+func (p *Book) increase(d *float32) float32 {
+	p.Price.salePrice = p.Price.price * (1 + *d)
+	return p.Price.salePrice
 }
--- methods/main.go
+++ methods/main.go
@@ -1,24 +1,43 @@
 package main
 
 func main() {
+
 	// Defind some books
 	book1 := Book{
 		Title:  "Pride and Prejudice",
 		Author: "Jane Austen",
-		Price:  35.95,
+		Price: price{
+			price:     10,
+			priceUp:   0.0,
+			priceDn:   .2,
+			salePrice: 0.0,
+		},
 	}
+	book1.Price.salePrice = book1.discount(&book1.Price.priceDn)
+
 	book2 := Book{
 		Title:  "1984",
 		Author: "George Orwell",
-		Price:  38.95,
+		Price: price{
+			price:   38.95,
+			priceUp: 0.0,
+			priceDn: .5,
+		},
 	}
+	book2.Price.salePrice = book2.discount(&book2.Price.priceDn)
+
 	book3 := Book{
 		Title:  "War of Peace",
 		Author: "Leo Tolstoy",
-		Price:  28.95,
+		Price: price{
+			price:   28.95,
+			priceUp: .2,
+			priceDn: 0,
+		},
 	}
+	book3.Price.salePrice = book3.increase(&book3.Price.priceUp)
 
-	books := ListBooks{book1, book2, book3}
+	books := ListBooks{&book1, &book2, &book3}
 
 	// Print one book at a time
 	book1.PrintItem()
