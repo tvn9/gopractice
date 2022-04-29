da6ca823c5d9b833dd4a1aba862855e1c49d7970
Thanh Nguyen <chrisng9@outlook.com>
2022-04-28 23:17:20 -0700

    Adding Array exercices

--- exercises/array/exer1/main.go
+++ exercises/array/exer1/main.go
@@ -27,7 +27,7 @@ type ShopList struct {
 
 func main() {
 	// Create a shopping list
-	list := [...]ShopList{
+	list := [4]ShopList{
 		{
 			ItemNo:   1001,
 			ItemName: "Chicken",
@@ -43,17 +43,29 @@ func main() {
 			ItemName: "Ice Screem",
 			Price:    9.99,
 		},
-		{
-			ItemNo:   1004,
-			ItemName: "Butter",
-			Price:    9.99,
-		},
 	}
-	fmt.Println(list)
-	fmt.Println("Total number of item:", len(list))
 
+	totalItem := countItems(list)
+
+	// Print the list item on the list
+	fmt.Println(list[totalItem-1])
+
+	// Print total number of items
+	fmt.Println("Total number of item:", totalItem)
+
+	// Print the total cost
 	totalCost := calcTotal(list)
 	fmt.Printf("Total cost = %.2f\n", totalCost)
+
+	// Add a new item on the ShopList
+	list[3].ItemNo = 1004
+	list[3].Price = 12.99
+
+	// After adding a fourth item
+	fmt.Println("After adding the fourth item")
+
+	// Print list
+	printList(list)
 }
 
 func calcTotal(l [4]ShopList) float32 {
@@ -65,4 +77,35 @@ func calcTotal(l [4]ShopList) float32 {
 	}
 
 	return total
+}
+
+func countItems(list [4]ShopList) int {
+	total := 0
+
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+		if item.ItemName != "" || item.ItemNo > 0 {
+			total += 1
+		}
+	}
+	return total
+}
+
+func printList(list [4]ShopList) {
+	var (
+		totalItem int
+		totalCost float32
+	)
+
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+
+		if item.ItemName != "" || item.ItemNo > 0 {
+			totalItem += 1
+			totalCost += item.Price
+		}
+	}
+
+	fmt.Printf("Total number of items = %d\n", totalItem)
+	fmt.Printf("Total cost for all tems = %.2f\n", totalCost)
 }
