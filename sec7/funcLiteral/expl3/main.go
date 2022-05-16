02c40224963ba35ab0a043b0aff086c9e6b6afd9
Thanh Nguyen <chrisng9@outlook.com>
2022-05-16 15:38:10 -0700

    Adding function-leteral examples

--- sec7/funcLiteral/expl3/main.go
+++ sec7/funcLiteral/expl3/main.go
@@ -4,25 +4,32 @@ package main
 
 import "fmt"
 
+// calculatePrice func accept Closures func that return a float64
 func calculatePrice(sub float64, discFn func(sub float64) float64) float64 {
 	return sub - (sub * discFn(sub))
 }
 
 func main() {
-	msrp := 20.0
-	disc := 0.1
+	amntSpent := 200.0
+	discAllow := 100.0
+	discount := 0.3
+	// discFun is a Closures
 	discFn := func(sub float64) float64 {
+		var amnt float64
 		// Buy more save more!
-		if sub > 100.0 {
-			disc += 0.1
+		if sub > discAllow {
+			amnt += discount
 		}
 		// Max discount
-		if disc > 0.3 {
-			disc = 0.3
+		if amnt > 0.3 {
+			amnt = 0.3
 		}
-		return disc
+		fmt.Println("Discount amount", 100*amnt) // just to make sure the correct discount
+		return amnt
 	}
-	fmt.Println("Price: ", msrp)
-	totalPrice := calculatePrice(msrp, discFn)
+	disc := discFn(discount)               // => 0 (this is because Closures)
+	fmt.Println("Discount amount =", disc) // print out the discount amount
+	fmt.Println("Price: ", amntSpent)
+	totalPrice := calculatePrice(amntSpent, discFn)
 	fmt.Println("Total price after discount =", totalPrice)
 }
