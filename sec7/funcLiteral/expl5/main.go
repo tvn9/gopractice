8db7ce517bc3e81acf29f22192fe62a3f86ad046
Thanh Nguyen <chrisng9@outlook.com>
2022-05-16 16:28:54 -0700

    adding another closure function example

--- sec7/funcLiteral/expl5/main.go
+++ sec7/funcLiteral/expl5/main.go
@@ -3,27 +3,31 @@ package main
 
 import "fmt"
 
+// add func adds two numbers type int and return result
 func add(a, b int) int {
 	return a + b
 }
 
+// compute func receive closure func and return result from add func
 func compute(a, b int, op func(a, b int) int) int {
 	fmt.Printf("Compute %d & %d\n", a, b)
 	return op(a, b)
 }
 
 func main() {
+	// difine a, b variables of type in
 	a, b := 10, 8
+
+	// call/pass a, b, and "add(a, b, int) int" to compute func
 	r1 := compute(a, b, add)
 	fmt.Printf("Result: %d\n", r1)
 
-	fmt.Println("Compute", compute(a, b, func(a, b int) int {
-		return a - b
-	}))
+	// just different way to pass result of compute func to other function
+	fmt.Printf("%d - %d = %d\n", a, b, compute(a, b, func(a, b int) int { return a - b }))
 
-	mul := func(a, b int) int {
-		fmt.Printf("Multipling %d * %d = ", a, b)
-		return a * b
-	}
+	// define a closure func to multiply two numbers
+	mul := func(a, b int) int { fmt.Printf("Multipling %d * %d = ", a, b); return a * b }
+
+	// call compute func and pass, a, b and mul func
 	fmt.Println(compute(a, b, mul))
 }
